bfa4ee91b13acc6cdff0b76e98ac6f6b32eabeb2
thaxz <thaxzt@gmail.com>
2023-07-29 21:52:21 -0300

    feature(GameViewController): Presenting Scene

--- FruitNinja/Controllers/GameViewController.swift
+++ FruitNinja/Controllers/GameViewController.swift
@@ -13,30 +13,13 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        let scene = SKScene(size: CGSize(width: screenWidth, height: screenHeight))
+        scene.scaleMode = .aspectFill
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        skView.presentScene(scene)
     }
 
     override var prefersStatusBarHidden: Bool {
---
+++ FruitNinja/Types/Types.swift
@@ -0,0 +1,11 @@
+//
+//  Types.swift
+//  FruitNinja
+//
+//  Created by thaxz on 29/07/23.
+//
+
+import Foundation
+
+public let screenWidth: CGFloat = 2048.0
+public let screenHeight: CGFloat = 1536.0
