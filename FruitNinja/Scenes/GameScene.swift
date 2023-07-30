0730acd6062c2b4d326f5bfa84805e19673aeb53
thaxz <thaxzt@gmail.com>
2023-07-29 22:12:26 -0300

    feature(GameScene): Creating fruit and popup time

--- FruitNinja/Scenes/GameScene.swift
+++ FruitNinja/Scenes/GameScene.swift
@@ -11,11 +11,18 @@ import GameplayKit
 class GameScene: SKScene {
     
     // MARK: - Properties
+    var popupTime: Double = 0.9
     
     //MARK: - Lifecycle
     
     override func didMove(to view: SKView) {
         setupNodes()
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        run(.wait(forDuration: popupTime)){
+            self.tossHandler()
+        }
     }
     
    
@@ -27,6 +34,7 @@ extension GameScene {
     
     func setupNodes(){
         createBg()
+        tossHandler()
     }
     
     
@@ -41,6 +49,26 @@ extension GameScene {
         bg.position = CGPoint(x: frame.width/2, y: frame.height/2)
         bg.zPosition = -1
         addChild(bg)
+    }
+    
+}
+
+// MARK: Fruit
+
+extension GameScene {
+    
+    func tossHandler(){
+        // time to spawn fruit
+        popupTime *= 0.0991
+        createSprite()
+    }
+    
+    func createSprite(){
+        let sprite = SKSpriteNode(imageNamed: "fruit_2")
+        sprite.setScale(1.5)
+        sprite.name = "Fruit"
+        sprite.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(sprite)
     }
     
 }
