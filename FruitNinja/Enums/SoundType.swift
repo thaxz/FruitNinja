2548443bf05720b2db051856e6a54f14350d786c
thaxz <thaxzt@gmail.com>
2023-07-31 17:33:17 -0300

    feature(SoundEffects): Creating SoundType enum

---
+++ FruitNinja/Enums/SoundType.swift
@@ -0,0 +1,22 @@
+//
+//  SoundType.swift
+//  FruitNinja
+//
+//  Created by thaxz on 31/07/23.
+//
+
+import Foundation
+
+enum SoundType: String {
+    
+    case button = "button.wav"
+    case explosion = "explosion.wav"
+    case launch = "launch.wav"
+    case sliceBombFuse = "sliceBombFuse.wav"
+    case smoosh1 = "smoosh1.wav"
+    case smoosh2 = "smoosh2.wav"
+    case smoosh3 = "smoosh3.wav"
+    case whack = "whack.wav"
+    case wrong = "wrong.wav"
+    
+}
