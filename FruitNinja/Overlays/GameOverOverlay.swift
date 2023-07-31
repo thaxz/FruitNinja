6a95135e3cbd64e9a0d59404dca1ea581ea64ee5
thaxz <thaxzt@gmail.com>
2023-07-31 19:53:40 -0300

    feature(GameOverOverlay): Adding touches

--- FruitNinja/Overlays/GameOverOverlay.swift
+++ FruitNinja/Overlays/GameOverOverlay.swift
@@ -38,6 +38,16 @@ class GameOverOverlay: BaseOverlay {
     
     // MARK: Touches
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+    }
     
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+     super.touchesMoved(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+    }
     
 }
